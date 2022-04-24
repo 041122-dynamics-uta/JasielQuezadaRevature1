92ee5f6b746c0c5ce22555e15b97acc08deb1a88
jasiel <jasiel874@revature.net>
2022-04-24 12:00:48 -0400

    bash

--- bash/bashDemo.sh
+++ bash/bashDemo.sh
@@ -6,7 +6,7 @@ echo Please choose a function
 num1=$1
 num2=$2
 
-if [[ $# -eq 0 ]] ; then
+if [ $# == 0 ] ; then
 while true
 do
 #offer options
@@ -19,19 +19,21 @@ do
     read option
 
  #get option and solve
- if [[ $option -eq 1 ]] ; then
-    echo You chose to add the numbers             
+ #ADD
+ if [ $option == 1 ] ; then
+    echo You chose addition             
     echo Enter your first number
     read num1
     echo Enter your second number
-    read num2
-    echo $num1 + $num2  
+    read num2  
     echo "Press enter for answers"
     read 
     echo The sum of $num1 and $num2 is $((num1 + num2)) 
+    echo
 
- elif [[ $option -eq 2 ]] ; then
-    echo You chose to substract the numbers 
+#Subtract
+ elif [ $option == 2 ] ; then
+    echo You chose substraction 
     echo Enter your first number
     read num1
     echo Enter your second number
@@ -39,9 +41,11 @@ do
     echo "Press enter for answers"
     read 
     echo The difference of $num1 and $num2 is $((num1 - num2)) 
+    echo
 
- elif [[ $option -eq 3 ]] ; then
-    echo You chose to multiply the numbers 
+ #multiply
+ elif [ $option == 3 ] ; then
+    echo You chose to multiply 
     echo Pleae enter your first number
     read num1
     echo Pleae enter your second number
@@ -49,30 +53,31 @@ do
     echo "Press enter for answers"
     read 
     echo The differemce of $num1 and $num2 is $((num1 * num2))
-
- elif [[ $option -eq 4 ]] ; then
-    echo You chose to divide the numbers
+    echo
+ 
+ #divide
+ elif [ $option == 4 ] ; then
+    echo You chose division
     echo Please enter your first number
     read num1
     echo Please enter your second number
     read num2
     echo "Press enter for answers"
     read 
-    echo The differemce of $num1 and $num2 is $((num1 / num2)) 
-
- elif [[ $option -eq 5 ]] ; then
+    echo The difference of $num1 and $num2 is $((num1 / num2)) 
+    echo
+ #exit   
+ elif [ $option == 5 ] ; then
     exit
  else
     echo 
     echo Invalid option.. Please try again
-    echo  
-    fi
-done
-else
-    echo Invalid option 
+    echo
 end
+    fi
+done 
 fi
-    add() 
+        add() 
 	    { results=$((num1 + num2))
 	    end
 	}
@@ -85,18 +90,10 @@ fi
 	    end	
 	}		
 	divide()
-	{
-	if [ $num2 == 0 ]
-	then
-		echo Divide by 0 error ! Press enter to continue
-		read
-		inputnumbers
-		break
-	else
-		results=$((num1 / num2))
+	        { results=$((num1 / num2))
 		end
 
-    }
+        }
 else
     echo Invalid choice    
 
